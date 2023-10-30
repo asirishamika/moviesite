bacf8968e2f98c90549de7ae23d53b72eeb47fb4
Asiri shamika <107676071+asirishamika@users.noreply.github.com>
2023-10-30 22:16:48 +0530

    search functionality UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+import SearchIcon from "./search.svg"
 
 
 const API_URL="http://www.omdbapi.com?apikey=9b9d5957";
@@ -18,8 +19,13 @@ function App() {
   },[]);
 
   return (
-    <div className="App">
-     <h1>My APp</h1>
+    <div className="app">
+     <h1>Movie Hub</h1>
+     <div className='search'>
+      <input placeholder='Search the movies' value={""}  onChange={()=>{}}/>
+      <img src={SearchIcon} alt='search' onClick={()=>{}}/>
+     </div>
+     
     </div>
   );
 }
