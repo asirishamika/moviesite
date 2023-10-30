6be1f7774a1b18c5ae3007ff6dcd0faba4dc70d8
Asiri shamika <107676071+asirishamika@users.noreply.github.com>
2023-10-30 20:15:59 +0530

    check api is connecting properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
+const API_URL="http://www.omdbapi.com?apikey=9b9d5957";
+
 function App() {
- 
+ //9b9d5957 API Key
+ const searchMovies=async (title)=>{
+  const response =await fetch(`${API_URL}&s=${title}`);
+  const data=await response.json();
+
+  console.log(data.Search);}
+
+ useEffect(()=>{
+ searchMovies('Spiderman');
+  },[]);
 
   return (
     <div className="App">
      <h1>My APp</h1>
     </div>
-  )
+  );
 }
 
 export default App
